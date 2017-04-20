495b47990fb9a5ad4bd049147ebe23bfcd1b9d9d
Zexi Fang <zexifang@Zexis-MBP.home>
2017-04-20 20:06:39 +0100

    Analysis of the Airbnb data of London

--- airbnb.py
+++ airbnb.py
@@ -4,20 +4,45 @@ import xgboost as xgb
 from itertools import compress
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_selection import chi2
+from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 
+def transform_review_scores(df, resp='review_scores_rating'):
+    """Keep observations with review scores and transform them to five levels
+    
+    :param df: data frame to be transformed
+    :param resp: response
+    :return: transformed data frame
+    """
+    df = df[~df[resp].isnull()].reset_index(0, True)
+    df[resp] = pd.cut(df[resp], np.arange(0, 120, 20), labels=np.arange(5), include_lowest=True)
+    return df
+
+
 def transform_zipcode(df):
+    """Transform zipcode to outward code
+    
+    :param df: data frame to be transformed
+    :return: transformed data frame
+    """
     variable = 'zipcode'
     df[variable] = df[variable].fillna('')
     df[variable] = df[variable].str.upper().str.replace('[^0-9A-Z]+', '')
-    df.loc[df[variable].str.len() >= 8, variable] = ''
-    df.loc[df[variable].str.len() >= 5, variable] = df.loc[df[variable].str.len() >= 5, variable].str.slice(0, -3)
+    df.loc[df[variable].str.len() >= 8, variable] = ''  # Remove wrongly recorded zipcode
+    df.loc[df[variable].str.len() >= 5, variable] = df.loc[df[variable].str.len() >= 5, variable] \
+        .str.slice(0, -3)  # Extract outward code
     return df
 
 
 def transform_label(df, variables):
+    """Encode labels using numerical values
+    
+    :param df: data frame to be transformed
+    :param variables: list of variables to be encoded
+    :return: transformed data frame
+    """
     encoder = LabelEncoder()
     for variable in variables:
         df[variable] = df[variable].fillna('')
@@ -26,31 +51,55 @@ def transform_label(df, variables):
 
 
 def transform_host_since(df):
+    """Transform host start date to duration
+    
+    :param df: data frame to be transformed
+    :return: transformed data frame
+    """
     df['host_since'] = pd.to_datetime(df['host_since'], yearfirst=True)
-    df['host_for'] = (pd.Timestamp.now() - df['host_since']).dt.days
+    df['host_for'] = (df['host_since'].max() - df['host_since']).dt.days
     return df
 
 
 def transform_boolean(df, variables):
+    """Transform boolean values
+    
+    :param df: data frame to be transformed
+    :param variables: list of variables to be transformed
+    :return: transformed data frame
+    """
     for variable in variables:
         df[variable] = df[variable].map({'f': 0, 't': 1}, 'ignore')
     return df
 
 
-def transform_text(df, variables):
-    vectorizer = CountVectorizer(stop_words='english', max_features=100)
+def transform_text(df, variables, max_features=100):
+    """Transform variables consisting of text into a sparse format
+    
+    :param df: data frame to be transformed
+    :param variables: list of variables to be transformed
+    :param max_features: maximum number of features
+    :return: transformed data frame
+    """
+    vectorizer = CountVectorizer(stop_words='english', max_features=max_features)
     tmp = []
     for variable in variables:
         df[variable] = df[variable].fillna('')
         sparse = vectorizer.fit_transform(df[variable])
         sparse = pd.DataFrame(sparse.toarray(), columns=sorted(vectorizer.vocabulary_.keys())) \
-            .add_prefix(variable + '.')
+            .add_prefix(variable + '.')  # Rename the extracted features
         tmp.append(sparse)
     df = pd.DataFrame(pd.concat([df] + tmp, 1))
     return df
 
 
 def transform_percent(df, variables):
+    """Transform strings of percentages to decimals
+    
+    :param df: data frame to be transformed
+    :param variables: list of variables to be transformed
+    :return: transformed data frame
+    """
     for variable in variables:
         df[variable] = df[variable].str.strip('%')
         df[variable] = df[variable].astype(np.float64) / 100
@@ -58,37 +107,59 @@ def transform_percent(df, variables):
 
 
 def transform_price(df, variables):
+    """Transform strings of price to numerals
+    
+    :param df: data framed to be transformed
+    :param variables: list of variables to be transformed
+    :return: transformed data frame
+    """
     for variable in variables:
-        df[variable] = df[variable].str.strip('$').str.replace(',', '')
+        df[variable] = df[variable].str.strip('$').str.replace(',', '')  # Remove dollar signs and thousands separators
         df[variable] = df[variable].astype(np.float64)
     return df
 
 
 def transform_amenities(df):
+    """Extract amenities
+    
+    :param df: data frame to be transformed
+    :return: transformed data frame
+    """
     variable = 'amenities'
     df[variable] = df[variable].str.replace(r'[:\-\./ ]', '_').str.replace(r'[\(\)]', '').str.replace(r'_+', '_') \
-        .str.lower()
+        .str.lower()  # Clean the format of amenities
     df = transform_text(df, [variable])
     columns_to_remove = [column for column in df.columns if 'missing' in column]
-    df = pd.DataFrame(df.drop(columns_to_remove, 1))
+    df = pd.DataFrame(df.drop(columns_to_remove, 1))  # Remove unwanted amenities
     return df
 
 
-def remove_redundant_features(df, variables, resp):
+def remove_redundant_features(df, variables, resp='review_scores_rating'):
+    """Perform chi-squared test to remove sparse features of low significance
+    
+    :param df: data frame to be transformed
+    :param variables: list of variables that have been transformed into a sparse format to be transformed
+    :param resp: response
+    :return: transformed data frame
+    """
     for variable in variables:
-        variables_list = [item for item in df.columns if variable + '.' in item]
+        variables_list = [item for item in df.columns if variable + '.' in item]  # Find sparse features
         tmp = df[variables_list + [resp]].dropna()
         _, p_val = chi2(tmp[variables_list], tmp[resp])
-        variables_list = list(compress(variables_list, (p_val > 0.05)))
+        variables_list = list(compress(variables_list, (p_val > 0.05)))  # Find sparse features of low significance
         df = df.drop(variables_list, 1)
     return df
 
 
-def transform_listings(df, resp):
+def transform_listings(df, resp='review_scores_rating'):
+    """Transform listings data
+    
+    :param df: data frame (listings data) to be transformed
+    :param resp: response
+    :return: transformed data frame
+    """
     df = df.rename(columns={'id': 'listing_id'})
-    df = df[~df[resp].isnull()].reset_index(0, True)
-    df[resp] = pd.cut(df[resp], np.arange(0, 120, 20), labels=np.arange(5), include_lowest=True)
-
+    df = transform_review_scores(df, resp)
     df = transform_zipcode(df)
     df = transform_label(df, ['experiences_offered',
                               'host_response_time',
@@ -194,73 +265,17 @@ def transform_listings(df, resp):
     return df
 
 
-def compute_lambda(count, k=5, f=5):
-    return 1 / (1 + np.exp(-(count - k) / f))
-
-
-def compute_prob(row):
-    prior, posterior, lambda_val = row
-    return lambda_val * posterior + (1 - lambda_val) * prior
-
-
-def reformat(num, name, resp, variable):
-    num = num.pivot(index=variable, columns=resp, values=variable + '.' + name).reset_index().fillna(0)
-    columns = dict(zip(np.arange(5), [variable + '.' + name + '.' + score for score in np.arange(5).astype(str)]))
-    num = num.rename(columns=columns)
-    return num
-
-
-def add_noise(df, tmp, variable, r=0.01):
-    df = df.merge(tmp, 'left').drop(variable, 1)
-    noise = 1 + np.random.uniform(-0.5, 0.5, (len(df), 5)) * r
-    df[[variable + '.prob.' + str(i) for i in np.arange(5)]] *= noise
-    return df
-
-
-def transform_high_cardinality(train, test, resp, variables):
-    prior = train[resp].value_counts(True).sort_index().reset_index()
-    prior['index'] = prior['index'].map(lambda x: 'prior.' + str(x))
-    prior = prior.transpose()
-    prior = prior.rename(columns=prior.iloc[0]).drop('index', 0).reset_index()
-
-    for variable in variables:
-        new_prior = pd.DataFrame(pd.concat([prior] * len(train[variable].unique()), ignore_index=True))
-        new_prior['index'] = train[variable].unique()
-        new_prior = new_prior.rename(columns={'index': variable})
-
-        posterior = train.groupby(variable)[resp].value_counts(True).rename(variable + '.posterior').reset_index()
-        posterior = reformat(posterior, 'posterior', resp, variable)
-
-        count = train.groupby([variable, resp])[resp].count().rename(variable + '.count').reset_index()
-        count[resp] = count[resp].astype(np.int64)
-        count = reformat(count, 'count', resp, variable)
-
-        tmp = new_prior.merge(posterior).merge(count)
-
-        for i in np.arange(5):
-            tmp[variable + '.lambda.' + str(i)] = tmp[variable + '.count.' + str(i)].map(compute_lambda)
-            tmp[variable + '.prob.' + str(i)] = tmp[['prior.' + str(i),
-                                                     variable + '.posterior.' + str(i),
-                                                     variable + '.lambda.' + str(i)]].apply(compute_prob, 1)
-
-        tmp = tmp[[variable] + [variable + '.prob.' + str(i) for i in np.arange(5)]]
-
-        train = add_noise(train, tmp, variable)
-        test = add_noise(test, tmp, variable)
-
-    return train, test
-
-
 def transform(df):
+    """Transform the data frame and split it into the train and test sets
+    
+    :param df: data frame (listings) to be transformed
+    :return: split train and test sets
+    """
     resp = 'review_scores_rating'
 
     df = transform_listings(df, resp)
 
     train, test = train_test_split(df)
-    train = train.reset_index(0, True)
-    test = test.reset_index(0, True)
-
-    train, test = transform_high_cardinality(train, test, resp, ['host_id', 'zipcode'])
 
     x_train = train.drop(resp, 1)
     y_train = train[resp]
@@ -278,20 +293,23 @@ def main():
     params = {'silent': 1,
               'eta': 0.05,
               'max_depth': 5,
+              'max_delta_step': 1,
               'subsample': 0.75,
               'colsample_bytree': 0.75,
-              'objective': 'multi:softmax',
+              'objective': 'multi:softprob',
               'num_class': 5,
               'eval_metric': 'mlogloss'}
     d_train = xgb.DMatrix(x_train, y_train)
-    num_boost_round = 1000
+    num_boost_round = 100
 
     bst = xgb.train(params, d_train, num_boost_round)
 
     d_test = xgb.DMatrix(x_test)
     pred = bst.predict(d_test)
 
-    print(pred)
+    m_log_loss = log_loss(y_test, pred, labels=np.arange(5))
+
+    print(m_log_loss)
 
 
 if __name__ == '__main__':

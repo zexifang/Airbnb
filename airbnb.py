acb42b3c7ed8aa2e5f27b7e8922ab2cdc8c38341
Zexi Fang <zexifang@Zexis-MBP.home>
2017-05-30 00:08:06 +0100

    Analysis of the Airbnb data of London

--- airbnb.py
+++ airbnb.py
@@ -1,12 +1,21 @@
+"""Main module.
+
+This module is the prediction of the review scores of Airbnb data.
+"""
+
+
 import numpy as np
 import pandas as pd
-import xgboost as xgb
+
 from itertools import compress
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_selection import chi2
 from sklearn.metrics import log_loss
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import Imputer
+from sklearn.utils import resample
 
 
 def transform_review_scores(df, resp='review_scores_rating'):
@@ -16,8 +25,10 @@ def transform_review_scores(df, resp='review_scores_rating'):
     :param resp: response
     :return: transformed data frame
     """
-    df = df[~df[resp].isnull()].reset_index(0, True)
-    df[resp] = pd.cut(df[resp], np.arange(0, 120, 20), labels=np.arange(5), include_lowest=True)
+    df = df.copy()
+    df = df[~df[resp].isnull()].reset_index(0, True)  # Remove missing values
+    df[resp] = pd.cut(df[resp], np.arange(0, 120, 20), labels=np.arange(5), include_lowest=True) \
+        # Bin response into five levels
     return df
 
 
@@ -28,11 +39,11 @@ def transform_zipcode(df, variable='zipcode'):
     :param variable: zipcode
     :return: transformed data frame
     """
-    df[variable] = df[variable].fillna('')
-    df[variable] = df[variable].str.upper().str.replace('[^0-9A-Z]+', '')
-    df.loc[df[variable].str.len() >= 8, variable] = ''  # Remove wrongly recorded zipcode
-    df.loc[df[variable].str.len() >= 5, variable] = df.loc[df[variable].str.len() >= 5, variable] \
-        .str.slice(0, -3)  # Extract outward code
+    df = df.copy()
+    df[variable] = df[variable].fillna('').str.lower().str.replace(r' +', '')  # Reformat zipcode
+    pat = df[variable].str.match(r'^[a-z]{1,2}[0-9][a-z0-9]?([0-9][a-z]{2})?$')  # Find zipcode of correct format
+    df.loc[~pat, variable] = ''  # Remove wrongly recorded zipcode
+    df[variable] = df[variable].str.extract(r'([a-z]{1,2})', expand=False)  # Extract postcode area
     return df
 
 
@@ -43,12 +54,30 @@ def transform_label(df, variables=None):
     :param variables: list of variables to be encoded
     :return: transformed data frame
     """
+    df = df.copy()
     if variables is None:
         variables = []
-    encoder = LabelEncoder()
+
+    label_binarizer = LabelBinarizer()
+    transformed_list = []
+
     for variable in variables:
-        df[variable] = df[variable].fillna('')
-        df[variable] = encoder.fit_transform(df[variable])
+        df[variable] = df[variable].fillna('')  # Reformat variables
+        transformed = label_binarizer.fit_transform(df[variable])  # Binarize variables
+
+        # Set column names of the transformed data
+        if transformed.shape[1] == 1:
+            transformed_columns = [variable]
+        else:
+            columns = pd.Series(label_binarizer.classes_).str.lower().str.replace(r'[&\-/ ]', '_') \
+                .str.replace(r'_+', '_')
+            transformed_columns = [variable + '.' + column for column in columns]
+
+        transformed = pd.DataFrame(transformed, columns=transformed_columns)
+        transformed_list.append(transformed)  # Add transformed data to the transformed list
+        df = df.drop(variable, 1)  # Remove original data
+
+    df = pd.DataFrame(pd.concat([df] + transformed_list, 1))  # Concatenate transformed data
     return df
 
 
@@ -58,22 +87,9 @@ def transform_host_since(df):
     :param df: data frame to be transformed
     :return: transformed data frame
     """
-    df['host_since'] = pd.to_datetime(df['host_since'], yearfirst=True)
-    df['host_for'] = (df['host_since'].max() - df['host_since']).dt.days
-    return df
-
-
-def transform_boolean(df, variables=None):
-    """Transform boolean values
-    
-    :param df: data frame to be transformed
-    :param variables: list of variables to be transformed
-    :return: transformed data frame
-    """
-    if variables is None:
-        variables = []
-    for variable in variables:
-        df[variable] = df[variable].map({'f': 0, 't': 1}, 'ignore')
+    df = df.copy()
+    df['host_since'] = pd.to_datetime(df['host_since'], yearfirst=True)  # Convert to datetime format
+    df['host_for'] = (df['host_since'].max() - df['host_since']).dt.days  # Calculate the duration of hosting
     return df
 
 
@@ -85,17 +101,26 @@ def transform_text(df, variables=None, max_features=100):
     :param max_features: maximum number of features
     :return: transformed data frame
     """
+    df = df.copy()
     if variables is None:
         variables = []
-    vectorizer = CountVectorizer(stop_words='english', max_features=max_features)
-    tmp = []
+
+    count_vectorizer = CountVectorizer(stop_words='english', max_features=max_features)
+    transformed_list = []
+
     for variable in variables:
         df[variable] = df[variable].fillna('')
-        sparse = vectorizer.fit_transform(df[variable])
-        sparse = pd.DataFrame(sparse.toarray(), columns=sorted(vectorizer.vocabulary_.keys())) \
-            .add_prefix(variable + '.')  # Rename the extracted features
-        tmp.append(sparse)
-    df = pd.DataFrame(pd.concat([df] + tmp, 1))
+        transformed = count_vectorizer.fit_transform(df[variable])
+
+        # Set column names of the transformed data
+        columns = sorted(count_vectorizer.vocabulary_.keys())
+        transformed_columns = [variable + '.' + column for column in columns]
+
+        transformed = pd.DataFrame(transformed.toarray(), columns=transformed_columns)
+        transformed_list.append(transformed)  # Add transformed data to the transformed list
+        df = df.drop(variable, 1)  # Remove original data
+
+    df = pd.DataFrame(pd.concat([df] + transformed_list, 1))  # Concatenate transformed data
     return df
 
 
@@ -106,6 +131,7 @@ def transform_percent(df, variables=None):
     :param variables: list of variables to be transformed
     :return: transformed data frame
     """
+    df = df.copy()
     if variables is None:
         variables = []
     for variable in variables:
@@ -121,6 +147,7 @@ def transform_price(df, variables=None):
     :param variables: list of variables to be transformed
     :return: transformed data frame
     """
+    df = df.copy()
     if variables is None:
         variables = []
     for variable in variables:
@@ -136,30 +163,26 @@ def transform_amenities(df, variable='amenities'):
     :param variable: amenities
     :return: transformed data frame
     """
+    df = df.copy()
     df[variable] = df[variable].str.replace(r'[:\-\./ ]', '_').str.replace(r'[\(\)]', '').str.replace(r'_+', '_') \
-        .str.lower()  # Clean the format of amenities
-    df = transform_text(df, [variable])
+        .str.lower()  # Reformat amenities
+    df = transform_text(df, [variable])  # Transform amenities using transform_text
     columns_to_remove = [column for column in df.columns if 'missing' in column]
     df = pd.DataFrame(df.drop(columns_to_remove, 1))  # Remove unwanted amenities
     return df
 
 
-def remove_redundant_features(df, variables=None, resp='review_scores_rating'):
-    """Perform chi-squared test to remove sparse features of low significance
-    
-    :param df: data frame to be transformed
-    :param variables: list of variables that have been transformed into a sparse format to be transformed
-    :param resp: response
-    :return: transformed data frame
+def impute(df):
+    """Impute missing values
+
+    :param df: data frame to be imputed
+    :return: imputed data frame
     """
-    if variables is None:
-        variables = []
-    for variable in variables:
-        variables_list = [item for item in df.columns if variable + '.' in item]  # Find sparse features
-        tmp = df[variables_list + [resp]].dropna()
-        _, p_val = chi2(tmp[variables_list], tmp[resp])
-        variables_list = list(compress(variables_list, (p_val > 0.05)))  # Find sparse features of low significance
-        df = df.drop(variables_list, 1)
+    df = df.copy()
+    imputer = Imputer(strategy='median')  # Replace missing values with the median
+    columns = df.columns
+    df = imputer.fit_transform(df)
+    df = pd.DataFrame(df, columns=columns)
     return df
 
 
@@ -171,109 +194,154 @@ def transform_listings(df, resp='review_scores_rating'):
     :return: transformed data frame
     """
     df = df.rename(columns={'id': 'listing_id'})
+
     df = transform_review_scores(df, resp)
+
     df = transform_zipcode(df)
-    df = transform_label(df, ['experiences_offered',
-                              'host_response_time',
-                              'zipcode',
-                              'property_type',
-                              'room_type',
-                              'bed_type',
-                              'cancellation_policy'])
+
+    vars_label = [
+        'experiences_offered',
+        'host_response_time',
+        'zipcode',
+        'property_type',
+        'room_type',
+        'bed_type',
+        'cancellation_policy',
+        'host_is_superhost',
+        'is_location_exact',
+        'requires_license',
+        'instant_bookable',
+        'require_guest_profile_picture',
+        'require_guest_phone_verification'
+    ]
+    df = transform_label(df, vars_label)
+
     df = transform_host_since(df)
-    df = transform_boolean(df, ['host_is_superhost',
-                                'host_has_profile_pic',
-                                'host_identity_verified',
-                                'is_location_exact',
-                                'requires_license',
-                                'instant_bookable',
-                                'require_guest_profile_picture',
-                                'require_guest_phone_verification'])
+
     df = transform_text(df, ['host_verifications'])
+
     df = transform_percent(df, ['host_response_rate'])
+
     df = transform_price(df, ['price'])
+
     df = transform_amenities(df)
-    df = remove_redundant_features(df, ['host_verifications', 'amenities'], resp)
 
-    df = df.drop(['listing_id',
-                  'listing_url',
-                  'scrape_id',
-                  'last_scraped',
-                  'name',
-                  'summary',
-                  'space',
-                  'description',
-                  'neighborhood_overview',
-                  'notes',
-                  'transit',
-                  'access',
-                  'interaction',
-                  'house_rules',
-                  'thumbnail_url',
-                  'medium_url',
-                  'picture_url',
-                  'xl_picture_url',
-                  'host_url',
-                  'host_name',
-                  'host_since',
-                  'host_location',
-                  'host_about',
-                  'host_acceptance_rate',
-                  'host_thumbnail_url',
-                  'host_picture_url',
-                  'host_neighbourhood',
-                  'host_listings_count',
-                  'host_total_listings_count',
-                  'host_verifications',
-                  'host_has_profile_pic',
-                  'host_identity_verified',
-                  'street',
-                  'neighbourhood',
-                  'neighbourhood_cleansed',
-                  'neighbourhood_group_cleansed',
-                  'city',
-                  'state',
-                  'market',
-                  'smart_location',
-                  'country_code',
-                  'country',
-                  'latitude',
-                  'longitude',
-                  'is_location_exact',
-                  'amenities',
-                  'square_feet',
-                  'weekly_price',
-                  'monthly_price',
-                  'security_deposit',
-                  'cleaning_fee',
-                  'guests_included',
-                  'extra_people',
-                  'minimum_nights',
-                  'maximum_nights',
-                  'calendar_updated',
-                  'has_availability',
-                  'availability_30',
-                  'availability_60',
-                  'availability_90',
-                  'availability_365',
-                  'calendar_last_scraped',
-                  'first_review',
-                  'last_review',
-                  'review_scores_accuracy',
-                  'review_scores_cleanliness',
-                  'review_scores_checkin',
-                  'review_scores_communication',
-                  'review_scores_location',
-                  'review_scores_value',
-                  'requires_license',
-                  'license',
-                  'jurisdiction_names',
-                  'instant_bookable',
-                  'cancellation_policy',
-                  'require_guest_profile_picture',
-                  'require_guest_phone_verification',
-                  'reviews_per_month'], 1)
+    vars_drop = [
+        'listing_id',
+        'listing_url',
+        'scrape_id',
+        'last_scraped',
+        'name',
+        'summary',
+        'space',
+        'description',
+        'neighborhood_overview',
+        'notes',
+        'transit',
+        'access',
+        'interaction',
+        'house_rules',
+        'thumbnail_url',
+        'medium_url',
+        'picture_url',
+        'xl_picture_url',
+        'host_url',
+        'host_name',
+        'host_since',
+        'host_location',
+        'host_about',
+        'host_acceptance_rate',
+        'host_thumbnail_url',
+        'host_picture_url',
+        'host_neighbourhood',
+        'host_listings_count',
+        'host_total_listings_count',
+        'host_has_profile_pic',
+        'host_identity_verified',
+        'street',
+        'neighbourhood',
+        'neighbourhood_cleansed',
+        'neighbourhood_group_cleansed',
+        'city',
+        'state',
+        'market',
+        'smart_location',
+        'country_code',
+        'country',
+        'latitude',
+        'longitude',
+        'is_location_exact',
+        'square_feet',
+        'weekly_price',
+        'monthly_price',
+        'security_deposit',
+        'cleaning_fee',
+        'guests_included',
+        'extra_people',
+        'minimum_nights',
+        'maximum_nights',
+        'calendar_updated',
+        'has_availability',
+        'availability_30',
+        'availability_60',
+        'availability_90',
+        'availability_365',
+        'calendar_last_scraped',
+        'first_review',
+        'last_review',
+        'review_scores_accuracy',
+        'review_scores_cleanliness',
+        'review_scores_checkin',
+        'review_scores_communication',
+        'review_scores_location',
+        'review_scores_value',
+        'requires_license',
+        'license',
+        'jurisdiction_names',
+        'instant_bookable',
+        'require_guest_profile_picture',
+        'require_guest_phone_verification',
+        'reviews_per_month'
+    ]
+    df = pd.DataFrame(df.drop(vars_drop, 1))
 
+    df = impute(df)
+
+    return df
+
+
+def remove_redundant_features(train, test, variables=None, resp='review_scores_rating'):
+    """Perform chi-squared test to remove sparse features of low significance
+
+    :param train: train data frame to be transformed
+    :param test: test data frame to be transformed
+    :param variables: list of ``sparse`` variables to be transformed
+    :param resp: response
+    :return: transformed data frame
+    """
+    train = train.copy()
+    test = test.copy()
+    if variables is None:
+        variables = []
+
+    for variable in variables:
+        variables_list = [item for item in train.columns if variable + '.' in item]  # Find sparse features
+        tmp = train[variables_list + [resp]]  # Form data frame of sparse feature and response
+        _, p_val = chi2(tmp[variables_list], tmp[resp])  # Perform chi-squared test
+        variables_list = list(compress(variables_list, (p_val > 0.05)))  # Find sparse features of low significance
+        train = train.drop(variables_list, 1)  # Remove sparse features of low significance
+        test = test.drop(variables_list, 1)  # Remove sparse features of low significance
+    return train, test
+
+
+def upsample(df, resp='review_scores_rating'):
+    df = df.copy()
+    df_upsampled_list = []
+    max_n_samples = df[resp].value_counts().max()  # Find the class with maximum number of samples
+    for i in np.arange(5):
+        upsampled = resample(df[df[resp] == i], n_samples=max_n_samples)  # Upsample
+        df_upsampled_list.append(upsampled)  # Add upsampled data to the transformed list
+    df = pd.DataFrame(pd.concat(df_upsampled_list, ignore_index=True))  # Concatenate upsampled data
     return df
 
 
@@ -285,10 +353,16 @@ def transform(df):
     """
     resp = 'review_scores_rating'
 
-    df = transform_listings(df, resp)
+    df = transform_listings(df, resp)  # Perform feature engineering to listings data
 
-    train, test = train_test_split(df)
+    train, test = train_test_split(df)  # Split transformed data into train and test sets
 
+    train, test = remove_redundant_features(train, test, ['host_verifications', 'amenities']) \
+        # Remove redundant features using the train set
+
+    train = upsample(train)  # Upsample imbalanced classes
+
+    # Split train and test sets into features and response
     x_train = train.drop(resp, 1)
     y_train = train[resp]
     x_test = test.drop(resp, 1)
@@ -297,29 +371,28 @@ def transform(df):
     return x_train, y_train, x_test, y_test
 
 
+def fit_and_predict(x_train, y_train, x_test, y_test):
+    """Fit the random forest classifier and predict the review scores
+
+    :param x_train: train set features
+    :param y_train: train set response
+    :param x_test: test set features
+    :param y_test: test set response
+    :return: logarithmic loss of the test set
+    """
+    clf = RandomForestClassifier(500)
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict_proba(x_test)
+    m_log_loss = log_loss(y_test, y_pred)
+    return m_log_loss
+
+
 def main():
-    listings = pd.read_csv('../src/data_sets/listings.csv.gz')
+    listings = pd.read_csv('../src/data_sets/listings.csv.gz')  # Read data
 
-    x_train, y_train, x_test, y_test = transform(listings)
+    x_train, y_train, x_test, y_test = transform(listings)  # Transform data
 
-    params = {'silent': 1,
-              'eta': 0.05,
-              'max_depth': 5,
-              'max_delta_step': 1,
-              'subsample': 0.75,
-              'colsample_bytree': 0.75,
-              'objective': 'multi:softprob',
-              'num_class': 5,
-              'eval_metric': 'mlogloss'}
-    d_train = xgb.DMatrix(x_train, y_train)
-    num_boost_round = 100
-
-    bst = xgb.train(params, d_train, num_boost_round)
-
-    d_test = xgb.DMatrix(x_test)
-    pred = bst.predict(d_test)
-
-    m_log_loss = log_loss(y_test, pred, labels=np.arange(5))
+    m_log_loss = fit_and_predict(x_train, y_train, x_test, y_test)  # fit and predict
 
     print('The logarithmic loss is {}.'.format(m_log_loss))
 
